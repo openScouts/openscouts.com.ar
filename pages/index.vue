27c268f39ebae7d4ba5e53aadd1561766baaeb5a
Andres Hal <caseroft@gmail.com>
2019-10-01 13:41:18 -0300

    Subo Logo

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
+      <img src="/logo.png" />
+    </v-flex>
+    <div>
+      Proximamente
+    <div/>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  components: {}
+}
+</script>
